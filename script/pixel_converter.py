c7e1fb7bf0ef724a516115e45c60752a283a8588
JingShing <JingShingLai@gmail.com>
2022-09-06 09:01:02 +0800

    Add files via upload

--- script/pixel_converter.py
+++ script/pixel_converter.py
@@ -2,16 +2,24 @@ import os
 import re
 import cv2
 from pixel_transform import transform
-from settings import resource_path
+from gif_process import gif_edit
+from video_editor import video_edit
 
-def convert(img_path, k=16, scale=1, blur=0, erode=0,alpha = True, to_tw = False, dither=False, saturation=0, contrast=0):
+def convert(img_path, set_dict):
     # k is color num
     # scale is pixel size
     # to_tw means to twitter
     img_path.replace("\\", "/")
     img_file_name = re.split("/", img_path)[-1]
-    img_res = transform(img_path, k=k, scale=scale, blur=blur, erode=erode, alpha=alpha, to_tw=to_tw, dither=dither, saturation=saturation, contrast=contrast)
-    img_path = img_path.split(img_file_name)[0]
+    img_file_format = img_file_name.split('.')[-1]
+    if img_file_format in ['gif', 'GIF']:
+        img_res = gif_edit(img_path, set_dict)
+    elif img_file_format in ['mp4', 'avi', 'flv']:
+        img_res = video_edit(img_path, set_dict)
+    else:
+        img_res = transform(img_path, set_dict)
+        img_path = img_path.split(img_file_name)[0]
+    # cv2.imshow('show', img_res)
     return img_res
 
 def save_cv_img(img, img_path, img_name):
