f59ed13a1691bceb7db1e31941772e644f90005a
JingShing <JingShingLai@gmail.com>
2023-04-11 23:56:36 +0800

    Create prototype3.py

---
+++ test_prototype/canny_dither_sample/prototype3.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+# 讀取圖像
+img = cv2.imread('input_image.jpg')
+
+# 將圖像轉為灰度
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 使用Canny邊緣檢測
+edges = cv2.Canny(gray, 100, 200)
+
+# 定義有序抖動的閾值矩陣
+threshold = np.array([[0, 128], [192, 64]], dtype=np.uint8)
+
+# 使用有序抖動處理邊緣
+dithered_edges = cv2.threshold(edges, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+dithered_edges = cv2.resize(dithered_edges, None, fx=3, fy=3, interpolation=cv2.INTER_NEAREST)
+dithered_edges = dithered_edges.astype(np.uint8)  # 將掩模轉換為與圖像相同的深度
+for i in range(dithered_edges.shape[0]):
+    for j in range(dithered_edges.shape[1]):
+        if dithered_edges[i, j] > threshold[i % 2, j % 2]:
+            dithered_edges[i, j] = 255
+        else:
+            dithered_edges[i, j] = 0
+
+# 將處理後的邊緣合併到原圖像中
+output = cv2.bitwise_and(img, img, mask=dithered_edges)
+
+# 顯示輸出圖像
+cv2.imshow('output', output)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
